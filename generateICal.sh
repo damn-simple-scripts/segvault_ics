0282910f0c5f10471ec4f538ea19f392beec56c2
root <you@example.com>
2018-07-17 21:51:28 +0200

    complicated fix for start and end

--- generateICal.sh
+++ generateICal.sh
@@ -12,11 +12,11 @@ rm $tmp
 
 currentYear=$(date '+%Y')
 
-cat $tmp2 | sed -r "s/.*<div class=\"entry-date\">([^\.]+)[\.0-9-]*<span>(.*)<\/span><\/div> +<\/a>.*<h2><a href=\"#\">(.*)<\/a><\/h2>.*<span class=\"label label-.*\">(.*)<\/span><\/li>.*<i class=\"icon-time\"><\/i>([0-9 ]*):*([0-9 ]*).*<\/a><\/li> <li><a href=\"#\">.*class=\"icon-map-marker2\"><\/i>(.*)<\/a><\/li> <\/ul.*<div class=\"entry-content\"> <p>(.*)<\/p> <\/div> <\/div> <\/div.*/BEGIN:VEVENT\nUID:SEGVAULT$currentYear\2\1T\5\600\nSUMMARY:\3\nDTSTART;TZID=Europe\/Vienna:$currentYear\2\1T\5\6ZEROZERO\nDTEND;TZID=Europe\/Vienna:$currentYear\2\1T\5\659\nDTSTAMP:$currentYear\2\1T\5\600Z\nCATEGORIES:\4\nLOCATION:\7\nDESCRIPTION:\8\nEND:VEVENT/g" > $tmp
+cat $tmp2 | sed -r "s/.*<div class=\"entry-date\">([^\.]+)\.+-*([0-9]*)\.*<span>(.*)<\/span><\/div> +<\/a>.*<h2><a href=\"#\">(.*)<\/a><\/h2>.*<span class=\"label label-.*\">(.*)<\/span><\/li>.*<i class=\"icon-time\"><\/i>([0-9 ]*):*([0-9 ]*).*<\/a><\/li> <li><a href=\"#\">.*class=\"icon-map-marker2\"><\/i>(.*)<\/a><\/li> <\/ul.*<div class=\"entry-content\"> <p>(.*)<\/p> <\/div> <\/div> <\/div.*/BEGIN:VEVENT\nUID:SEGVAULT$currentYear\3\1T\6\700\nSUMMARY:\4\nDTSTART;TZID=Europe\/Vienna:$currentYear\3\1T\6\7ZEROZERO\nDTEND;TZID=Europe\/Vienna:$currentYear\3\2T\6\759\nDTSTAMP:$currentYear\3\1T\6\700Z\nCATEGORIES:\5\nLOCATION:\8\nDESCRIPTION:\9\nEND:VEVENT/g" > $tmp
 
 cat $tmp | sed 's/Mai/05/g' | sed 's/März/03/' | sed 's/Jänner/01/g'  | sed 's/April/04/g' | sed 's/Februar/02/g' | sed 's/J<C3><A4>nner/01/g' | sed 's/Juni/06/g' | sed -r 's/ *ZERO/0/g' | sed 's/Juli/07/g' | sed 's/August/08/g' | sed 's/September/09/g' | sed 's/Oktober/10/g' | sed 's/November/11/g' | sed 's/Dezember/12/g' >  $tmp2
 
-cat $tmp2 | sed 's/00 00$/0000/g' | sed -r 's/[ \t\f]$//g' | sed 's/$/<br>/' | sed 's/\.$//g' | sed 's/??/01/g'  | pandoc -f html -t plain | sed -r 's/:([0-9]{6})([0-9])$/:\10\2/g' |  sed -r 's/:([0-9]{6})([0-9])T/:\10\2T/g'| sed -r 's/([0-9]) 59/\159/g' | sed "s/$(echo '&#223;' | pandoc -f html -t plain)/ss/g" | sed "s/$(echo '&ouml;' | pandoc -f html -t plain)/oe/g" | sed "s/$(echo '&auml;' | pandoc -f html -t plain)/ae/g" | sed "s/$(echo '&uuml;' | pandoc -f html -t plain)/ue/g" > $tmp
+cat $tmp2 | sed 's/00 00$/0000/g' | sed -r 's/[ \t\f]$//g' | sed 's/$/<br>/' | sed 's/\.$//g' | sed 's/??/01/g'  | pandoc -f html -t plain | sed -r 's/:([0-9]{6})([0-9])$/:\10\2/g' |  sed -r 's/:([0-9]{6})([0-9])T/:\10\2T/g'| sed -r 's/([0-9]) 59/\159/g' | sed 's/T00$/T235959/g' | sed "s/$(echo '&#223;' | pandoc -f html -t plain)/ss/g" | sed "s/$(echo '&ouml;' | pandoc -f html -t plain)/oe/g" | sed "s/$(echo '&auml;' | pandoc -f html -t plain)/ae/g" | sed "s/$(echo '&uuml;' | pandoc -f html -t plain)/ue/g" > $tmp
 
 incremented=$(tempfile)
 last_hour="19"
@@ -56,7 +56,7 @@ echo "RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=10" >> $tmp2
 echo "END:STANDARD" >> $tmp2
 echo "END:VTIMEZONE" >> $tmp2
 
-cat $incremented  | tr "\n" "\r" | sed 's/\r\r*/\r/g'| sed -r 's/ \r/\r/g'| sed -r 's/\r([A-Z][A-Z][A-Z])/\n\1/g' | sed 's/\r$//g' | sed 's/\r/\\n/g' | sed 's/  */ /g' | iconv -c -t ascii >> $tmp2
+cat $incremented | sed 's/T59$/T235959/g' | sed -r 's/(DTSTART;.*T)235959/\1000000/g' | tr "\n" "\r" | sed 's/\r\r*/\r/g'| sed -r 's/ \r/\r/g'| sed -r 's/\r([A-Z][A-Z][A-Z])/\n\1/g' | sed 's/\r$//g' | sed 's/\r/\\n/g' | sed 's/  */ /g' | iconv -c -t ascii >> $tmp2
 rm $incremented
 
 echo -e "\nEND:VCALENDAR" >> $tmp2
