9d4cfe1d9aa73d22637453859619a42490cda4b7
root <you@example.com>
2019-03-25 13:15:37 +0100

    parse also json

--- generateICal.sh
+++ generateICal.sh
@@ -12,9 +12,9 @@ rm $tmp
 
 currentYear=$(date '+%Y')
 
-cat $tmp2 | sed -r "s/.*<div class=\"entry-date\">([^\.]+)\.*-*([0-9]*)\.*<span>(.*)<\/span><\/div> +<\/a>.*<h2><a href=\"#\">(.*)<\/a><\/h2>.*<span class=\"label label-.*\">(.*)<\/span><\/li>.*<i class=\"icon-time\"><\/i>[ab ]*([0-9 ]*):*([0-9 ]*).*<\/a><\/li> <li><a href=\"#\">.*class=\"icon-map-marker2\"><\/i>(.*)<\/a><\/li> <\/ul.*<div class=\"entry-content\"> <p>(.*)<\/p> <\/div> <\/div> <\/div.*/BEGIN:VEVENT\nUID:SEGVAULT$currentYear\3\1T\6\700\nSUMMARY:\4\nDTSTART;TZID=Europe\/Vienna:$currentYear\3\1T\6\7ZEROZERO\nDTEND;TZID=Europe\/Vienna:$currentYear\3\2T\6\759\nDTSTAMP:$currentYear\3\1T\6\700Z\nCATEGORIES:\5\nLOCATION:\8\nDESCRIPTION:\9\nEND:VEVENT/g" > $tmp
+cat $tmp2 | sed -r "s/.*<div class=\"entry-date\">([^\.]+)\.*-*([0-9]*)\.*<span>(.*)<\/span><\/div> +<\/a>.*<h2><a href=\"#\">(.*)<\/a><\/h2>.*<span class=\"label label-.*\">(.*)<\/span><\/li>.*<i class=\"icon-time\"><\/i>[ab ]*([0-9 ]*):*([0-9 ]*).*<\/a><\/li> <li><a href=\"#\">.*class=\"icon-map-marker2\"><\/i>(.*)<\/a><\/li> <\/ul.*<div class=\"entry-content\"> <p>(.*)<\/p> <\/div> <\/div> <\/div.*/BEGIN:VEVENT\nUID:SEGVAULT$currentYear\3\1T\6\700\nSUMMARY:\4\nDTSTART;TZID=Europe\/Vienna:$currentYear\3\1T\6\7ZEROZERO\nDTEND;TZID=Europe\/Vienna:$currentYear\3\2T\6\759\nDTSTAMP:$currentYear\3\1T\6\700Z\nCATEGORIES:\5\nLOCATION:\8\nDESCRIPTION:\9\nEND:VEVENT/g" | sed 's/\\nBinary file \(standard input\) matches//g' > $tmp
 
-cat $tmp | sed 's/Mai/05/g' | sed 's/März/03/' | sed 's/Jänner/01/g'  | sed 's/April/04/g' | sed 's/Februar/02/g' | sed 's/J<C3><A4>nner/01/g' | sed 's/Juni/06/g' | sed -r 's/ *ZERO/0/g' | sed 's/Juli/07/g' | sed 's/August/08/g' | sed 's/September/09/g' | sed 's/Septemeber/09/g' | sed 's/Oktober/10/g' | sed 's/November/11/g' | sed 's/Dezember/12/g' >  $tmp2
+cat $tmp | sed 's/�/ä/g' | sed 's/Mai/05/g' | sed 's/M�rz/03/g' | sed 's/März/03/g' | sed 's/Jänner/01/g'  | sed 's/April/04/g' | sed 's/Februar/02/g' | sed 's/J<C3><A4>nner/01/g' | sed 's/Juni/06/g' | sed -r 's/ *ZERO/0/g' | sed 's/Juli/07/g' | sed 's/August/08/g' | sed 's/September/09/g' | sed 's/Septemeber/09/g' | sed 's/Oktober/10/g' | sed 's/November/11/g' | sed 's/Dezember/12/g' >  $tmp2
 
 cat $tmp2 | sed 's/00 00$/0000/g' | sed -r 's/[ \t\f]$//g' | sed 's/$/<br>/' | sed 's/\.$//g' | sed 's/??/01/g'  | pandoc -f html -t plain | sed -r 's/:([0-9]{6})([0-9])$/:\10\2/g' |  sed -r 's/:([0-9]{6})([0-9])T/:\10\2T/g'| sed -r 's/([0-9]) 59/\159/g' | sed 's/T00$/T235959/g' | sed "s/$(echo '&#223;' | pandoc -f html -t plain)/ss/g" | sed "s/$(echo '&ouml;' | pandoc -f html -t plain)/oe/g" | sed "s/$(echo '&auml;' | pandoc -f html -t plain)/ae/g" | sed "s/$(echo '&uuml;' | pandoc -f html -t plain)/ue/g" | sed "s/$(echo '&Ouml;' | pandoc -f html -t plain)/Oe/g" | sed "s/$(echo '&Auml;' | pandoc -f html -t plain)/Ae/g" | sed "s/$(echo '&Uuml;' | pandoc -f html -t plain)/Ue/g"  > $tmp
 
@@ -69,9 +69,13 @@ echo "END:VTIMEZONE" >> $tmp2
 cat $incremented | sed 's/T00Z/T000000Z/g' | sed 's/T59$/T235959/g' | sed 's/00 00/0000/g' | sed -r 's/(DTSTART;.*T)235959/\1000000/g' | tr "\n" "\r" | sed -r 's/(DTSTAMP:[0-9]{8}T[0-9]{6})\r/\1Z/g' | sed 's/\r\r*/\r/g'| sed -r 's/ \r/\r/g'| sed -r 's/\r([A-Z][A-Z][A-Z])/\n\1/g' | sed 's/\r$//g' | sed 's/\r/\\n/g' | sed 's/  */ /g' | iconv -c -t ascii >> $tmp2
 rm $incremented
 
+echo "\n" >> $tmp2
+
+curl -s "https://segvault.space/js/events-data.js" | grep ":" | sed -r "s/^.*'([0-9][0-9])-([0-9][0-9])-([0-9][0-9][0-9][0-9])'\s*:\s*'[^>]*>([^<]*).*$/BEGIN:VEVENT\nUID:SEGVAULT\3\1\2T180000\nSUMMARY:\4\nDTSTART;TZID=Europe\/Vienna:\3\1\2T180000\nDTEND;TZID=Europe\/Vienna:\3\1\2T220000\nDTSTAMP:\3\1\2T180000Z\nCATEGORIES:Public\nLOCATION:Heinrich-Schneidmadl-Str. 15\\\, Top 0.23\\\, St. Poelten\\\, Austria\nDESCRIPTION: \4\\\nfrom https:\/\/segvault.space\/js\/events-data.js\nEND:VEVENT/g" | sed "s/$(echo '&#223;' | pandoc -f html -t plain)/ss/g" | sed "s/$(echo '&ouml;' | pandoc -f html -t plain)/oe/g" | sed "s/$(echo '&auml;' | pandoc -f html -t plain)/ae/g" | sed "s/$(echo '&uuml;' | pandoc -f html -t plain)/ue/g" | sed "s/$(echo '&Ouml;' | pandoc -f html -t plain)/Oe/g" | sed "s/$(echo '&Auml;' | pandoc -f html -t plain)/Ae/g" | sed "s/$(echo '&Uuml;' | pandoc -f html -t plain)/Ue/g" | sed 's/: */:/g'| iconv -c -t ascii  >> $tmp2
+
 echo -e "\nEND:VCALENDAR" >> $tmp2
 
-cat $tmp2 | sed '/^\s*$/d' | perl -pe 's/\n/\r\n/' > $tmp 2>>$outfile
+cat $tmp2 | sed 's/Binary file.*$//g' | sed 's/END:VEVENT.*/END:VEVENT/g' | sed '/^\s*$/d' | perl -pe 's/\n/\r\n/' > $tmp 2>>$outfile
 
 mv $tmp /var/www/html/segVaultCal/current.ics
 rm $tmp2
